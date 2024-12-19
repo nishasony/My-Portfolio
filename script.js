78adf89f8fb69bf7170e2f37bbb6f986c5e6d79d
nishasony <148679816+nishasony@users.noreply.github.com>
2024-12-19 16:16:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ MenuBtn.addEventListener('click', function(e){
 }
 )
 
-// Typing Effect
+// Send Data to WhatsApp
+function sendwhatsapp(){
+    
+    var name = document.querySelector('#name').value;
+    var email = document.querySelector('#email').value;
+    var subject = document.querySelector('#subject').value;
+    var message = document.querySelector('#smg').value;
 
+    var url = "https://wa.me/919117395865?text="
+    +"*Name :* " +name+"%0a"
+    +"*Email :* " +email+"%0a"
+    +"*Subject :* " +subject+"%0a"
+    +"*Message :* " +message+"%0a"
+    
+    window.open(url, '_blank').focus();
+}
